ef9de8de847adcd9a7fe757ba85c84a9211fbe4b
Renato Leme <rntreisleme@gmail.com>
2021-07-26 11:26:03 -0300

    Initial commit

---
+++ vblog.js
@@ -0,0 +1,16 @@
+app.component('vblog', {
+    props: {
+        posts: {
+            type: Array
+        }
+    },
+    template:
+    `<div class="generic-frame">
+<div class="generic-window">
+oi
+</div>
+</div>`,
+    methods: {
+        
+    }
+})
